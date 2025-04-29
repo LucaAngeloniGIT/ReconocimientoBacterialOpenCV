404184b277ee1daee3476840b3fcabc3a7ef5c83
LucaAngeloniGIT <angeloniluca07@gmail.com>
2025-04-29 17:16:17 -0300

    YOLOv11

---
+++ Deteccion con YoloV11/Test.py
@@ -0,0 +1,13 @@
+from ultralytics import YOLO
+
+model = YOLO("yolo11n-obb")
+
+results = model("Bac.jpg",show=True)
+
+for result in results:
+    xywh = result.boxes.xywh  # center-x, center-y, width, height
+    xywhn = result.boxes.xywhn  # normalized
+    xyxy = result.boxes.xyxy  # top-left-x, top-left-y, bottom-right-x, bottom-right-y
+    xyxyn = result.boxes.xyxyn  # normalized
+    names = [result.names[cls.item()] for cls in result.boxes.cls.int()]  # class name of each box
+    confs = result.boxes.conf  # confidence score of each box
